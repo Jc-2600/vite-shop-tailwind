8f55e76e144a50e6c13e6c5cc23947a36fc00e26
Jc-2600 <juan.lopez1@utp.edu.co>
2025-01-13 16:56:17 -0500

    Create card-box component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
+import { Card } from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
 
 export function Home() {
 
     return (
        <Layout>
-            <h1>Home Page</h1>
+        <h1>Home Page</h1>
+        <Card />
       </Layout>
     )
 }
