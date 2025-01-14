bdf9d77ee4ac79d08d62a66a3b55b9e6ab7f9cc1
Jc-2600 <juan.lopez1@utp.edu.co>
2025-01-14 08:10:03 -0500

    Create card for products grid

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,37 @@
-export const Card = () => {
+import PropTypes from "prop-types"
+import { useState } from "react"
+
+export const Card = ({data}) => {
+    const [imageError, setImageError] = useState(false)
+    const handleImageError = () => {
+        setImageError(true)
+    }
+  
     return(
-        <article className = 'bg-gray-300/30 cursor-pointer w-56 h-60 rounded-lg shadow-md'>
+        <article className = 'bg-gray-300/30 cursor-pointer w-56 h-max rounded-lg shadow-md'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-full text-black text-xs m-2 p-1'>Electronics</span>
-                <img  className = 'w-full h-full object-cover rounded-lg'src="https://images.pexels.com/photos/577769/pexels-photo-577769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="headphones" />
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-full text-black text-xs m-2 p-1'>{data.category.name}</span>
+                <img  className = 'w-full h-full object-cover rounded-lg'
+                    src={
+                        !imageError 
+                            ? data.images[0]
+                            : "https://i0.wp.com/mikeyarce.com/wp-content/uploads/2021/09/woocommerce-placeholder.png?ssl=1"
+                    } 
+                    alt={data.title} 
+                    onError={handleImageError}
+                />
                 <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
                     +
                 </button>
             </figure>
             <p className='flex justify-between px-2'>
-                <span className='text-sm font-light'>Headphones</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light'>{data.title}</span>
+                <span className='text-lg font-medium'>$ {data.price}</span>
             </p>
         </article>
     )
 }
+
+Card.propTypes = {
+    data: PropTypes.object.isRequired,
+};
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from "react"
 import { Card } from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
+import { apiUrl } from "../../api"
 
 export function Home() {
+
+    const [items, setItems] = useState(null)
+
+    useEffect(() =>{
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`${apiUrl}`)
+          const data = await response.json()
+          setItems(data)
+          console.log(data)
+        } catch (error) {
+          console.error(`Oh no, ocurrió un error: ${error}`);
+        }
+      }
+      fetchData()
+    },[])
 
     return (
        <Layout>
         <h1>Home Page</h1>
-        <Card />
+        <section className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map(item => (
+             <Card key ={item.id} data = {item}/>
+            ))
+          }
+        </section>
       </Layout>
     )
 }
